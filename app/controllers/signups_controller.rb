2f8f7370ca73fe5f6a53e70c02212efd9e75a7e4
Brian <bbattell86@gmail.com>
2015-11-27 20:47:54 -0500

    Ending project will add more database functionality in the next project more importantly passing json arguments

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -1,16 +1,46 @@
 class SignupsController < ApplicationController
-  def index
-    @signups = Signup.all
-  end
+  #def index
+  #  @signups = Signup.all
+  #end
 
   def new
     @signup = Signup.new
   end
 
+  #def create
+  #  @signup = Signup.new(signup_params)
+  #  if @signup.save
+  #    redirect_to '/thanks'
+  #  else
+  #    render 'new'
+  #  end
+  #end
 
-#  private
-#  def signup_params
-#    params.require(:email).permit(:content)
-#  end
+  def thanks
+  end
+
+  def index
+    render json: Note.all
+  end
+
+  # GET /notes/1.json
+  def show
+  end
+
+  # POST /notes.json
+  def create
+    @signup = Signup.new(signup_params)
+
+    if @signup.save
+      render json: @signup, status: :ok
+    else
+      render json: @signup.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def signup_params
+    params.require(:signup).permit(:email,:name)
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get '/about', to: 'about#about'
   get '/signups', to: 'signups#index'
   get '/new', to: 'signups#new'
+  get '/signups', to: 'signups#create'
+  get '/thanks', to: 'signups#thanks'
+post '/signups', to: 'signups#create'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
